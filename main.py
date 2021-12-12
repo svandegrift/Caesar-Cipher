579a1914ec79430886d17a4f8f3c19f72260ff49
Steven Vandegrift <svandegrift@icloud.com>
2021-12-12 16:38:24 -0600

    Reorganize Code

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',' ']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+  if cipher_direction == "decode": shift_amount *= -1
+  message = ""
+  for letter in text:
     position = alphabet.index(letter)
     new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
-  
-def decrypt(encrypted_text, shift_amount):
-  decoded_text = ""
-  for character in encrypted_text:
-    decoded_text += alphabet[alphabet.index(character) - shift_amount]
-  print(f"The encoded text is {decoded_text}")
-  
-if direction == "encode":
-  encrypt(text, shift)
-elif direction == "decode":
-  decrypt(text, shift)
+    message += alphabet[new_position]
+  print(f"The {cipher_direction}d text is '{message}'")
+
+caesar(text,shift,direction)
