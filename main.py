4ef698e8d6948186c0eedcda87b8919ffa1d7125
Steven Vandegrift <svandegrift@icloud.com>
2021-12-12 16:09:32 -0600

    Add Encrypt Function

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):    
+  cipher_text = ""
+  for character in plain_text:
+    cipher_text += alphabet[alphabet.index(character) + shift_amount]
+  print(f"The encoded text is {cipher_text}")
+encrypt(text,shift)
